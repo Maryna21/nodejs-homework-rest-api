6fcba05a046c265ef994e0b0c5d5d7b7b62370c2
Maryna21 <64320384+Maryna21@users.noreply.github.com>
2021-05-10 22:41:23 +0300

    переписала з mongoose

--- model/index.js
+++ model/index.js
@@ -1,50 +1,40 @@
-const db = require('./db')
-const { ObjectID } = require('mongodb');
-
-const getCollection = async (db,name) =>{
- const client = await db
- const collection = await client.db().collection(name)
- return collection
-}
+const Contacts = require('./schemas/contact')
 
 const listContacts = async () => {
-  const collection = await getCollection(db, 'contacts')
-  const results = await collection.find().toArray()
+  const results = await Contacts.find()
   return results
 }
 
-const getContactById = async (contactId) => {
-  const collection = await getCollection(db, 'contacts')
-  const objectId = new ObjectID(contactId)
-  const [result] = await collection.find({_id: objectId}).toArray()
+const getContactById = async (id) => {
+  const result = await Contacts.findById({_id: id})
   return result
 }
-// console.log(getContactById('cc034bd4-7143-4690-a69a-dd8a805d5b6a'));
+// console.log(getContactById('609818a6d65cc0da9352e592'));
 
 const removeContact = async (id) => {
-  const collection = await getCollection(db, 'contacts')
-  const objectId = new ObjectID(id)
-  const {value: result} = await collection.findOneAndDelete({_id: objectId })
-return result
+  const result = await Contacts.findByIdAndRemove({_id: id})
+  return result
 //  console.log('delete', record); 
 }
 
 const addContact = async (body) => {
-  const record = {
-    ...body
-  }
-  const collection = await getCollection(db, 'contacts')
-  const {ops: [result]} = await collection.insertOne(record)
-  return result
+  // try {
+    const result = await Contacts.create(body)
+    return result
+  // } catch (e) {
+  //   if(e.name === 'ValidationError'){
+  //     e.status = 400
+  //   }
+  //   throw e  
+  // }
+ 
 }
 
 const updateContact = async (id, body) => {
-  const collection = await getCollection(db, 'contacts')
-  const objectId = new ObjectID(id)
-  const {value: result} = await collection.findOneAndUpdate(
-    {_id: objectId },
-    {$set: body},
-    {returnOriginal: false}
+  const result = await Contacts.findByIdAndUpdate(
+    {_id: id},
+    {...body},
+    {new: true}
     )
   return result
 }
---
+++ model/schemas/contact.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const contactSchema = new Schema({
+    name: {
+            type: String,
+            required: [true, 'Set name for contact'],
+            },
+    email: {
+              type: String,
+            },
+    phone: {
+              type: String,
+            },
+    favorite: {
+              type: Boolean,
+              default: false,
+            },
+},
+{versionKey: false, 
+  timestamps: true, 
+  toObject: {virtuals: true, 
+    transform: function (doc,ret) {
+    delete ret._id
+    return ret
+  }
+}, 
+toJSON: {virtuals: true, 
+  transform: function (doc,ret) {
+  delete ret._id
+  return ret
+}
+}
+},
+
+) 
+
+contactSchema.path('name').validate((value)=>{
+    const re = /[A-Z]\w+/
+    return re.test(String(value))
+})
+
+contactSchema.virtual('strAge').get(function (){
+    return `${this.age} age`
+})
+
+const Contact = model('contact', contactSchema)
+
+module.exports = Contact
